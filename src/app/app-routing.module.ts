a5a4ae844647c16bed2cb31243fa6190514909ae
Dranaen <sachama_@hotmail.com>
2021-08-20 11:57:44 +0200

    accès forum

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AccueilComponent } from './accueil/accueil.component';
 import { BoutiqueComponent } from './boutique/boutique.component';
+import { ForumComponent } from './forum/forum.component';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   {component: BoutiqueComponent, path:'boutique'},
   {component: AccueilComponent, path:'Accueil'},
-  {component: LoginComponent, path:'login'}
+  {component: LoginComponent, path:'connexion'},
+  {component: ForumComponent, path:'forum'}
 ];
 
 @NgModule({
