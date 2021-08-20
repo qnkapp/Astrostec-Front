148da9e2772338b7e28cb2c8b2d6400bdc830d2f
Dranaen <sachama_@hotmail.com>
2021-08-20 10:25:39 +0200

    Accueil

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccueilComponent } from './accueil/accueil.component';
 import { BoutiqueComponent } from './boutique/boutique.component';
 
 const routes: Routes = [
-  {component: BoutiqueComponent, path:'boutique'}
+  {component: BoutiqueComponent, path:'boutique'},
+  {component: AccueilComponent, path:'Accueil'}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BoutiqueComponent } from './boutique/boutique.component';
+import { AccueilComponent } from './accueil/accueil.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BoutiqueComponent
+    BoutiqueComponent,
+    AccueilComponent
   ],
   imports: [
     BrowserModule,
