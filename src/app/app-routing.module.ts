8b7c1be2d21557ff51abf1d034a82815af669da7
TimoLeong <timothy.ivan.leong@gmail.com>
2021-08-20 10:46:39 +0200

    login skeleton

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BoutiqueComponent } from './boutique/boutique.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-  {component: BoutiqueComponent, path:'boutique'}
+  {component: BoutiqueComponent, path:'boutique'},
+  {component: LoginComponent, path:'login' }
 ];
 
 @NgModule({
