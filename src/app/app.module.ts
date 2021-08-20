32568dee9656dfe606e6baa4dfb4c1ea128b82c0
Quentin Lecroart <qnkapplication@gmail.com>
2021-08-20 09:22:34 +0200

    ajout boutique

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BoutiqueComponent } from './boutique/boutique.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BoutiqueComponent
   ],
   imports: [
     BrowserModule,
