2ae521f41113193e4f07c0dc84e1e1baaf8dd80e
Liives <l.clairville@epmistes.net>
2021-08-20 11:48:14 +0200

    ajout forum

---
+++ src/app/forum/forum.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-forum',
+  templateUrl: './forum.component.html',
+  styleUrls: ['./forum.component.css']
+})
+export class AccueilComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
