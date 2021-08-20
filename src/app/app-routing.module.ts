bab892b674f668186596154b13d7d0c8b4447b35
Dranaen <sachama_@hotmail.com>
2021-08-20 12:04:46 +0200

    catalogue

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AccueilComponent } from './accueil/accueil.component';
 import { BoutiqueComponent } from './boutique/boutique.component';
+import { CatalogueComponent } from './catalogue/catalogue.component';
 import { ForumComponent } from './forum/forum.component';
 import { LoginComponent } from './login/login.component';
 
@@ -9,7 +10,8 @@ const routes: Routes = [
   {component: BoutiqueComponent, path:'boutique'},
   {component: AccueilComponent, path:'Accueil'},
   {component: LoginComponent, path:'connexion'},
-  {component: ForumComponent, path:'forum'}
+  {component: ForumComponent, path:'forum'},
+  {component: CatalogueComponent, path:'catalogue'}
 ];
 
 @NgModule({
