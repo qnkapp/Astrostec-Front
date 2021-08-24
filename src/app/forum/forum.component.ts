49d974a8a1ab9582c6752eccffb7a6c3472b750f
Dranaen <sachama_@hotmail.com>
2021-08-24 17:06:45 +0200

    chat

--- src/app/forum/forum.component.ts
+++ src/app/forum/forum.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ForumService } from '../services/forum.service';
 
 @Component({
   selector: 'app-forum',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ForumComponent implements OnInit {
 
-  constructor() { }
+  constructor(public nav: ForumService) { }
 
   ngOnInit(): void {
+    this.nav.show();
   }
 
 }
