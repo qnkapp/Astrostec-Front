18187f56cebca5c437a2c2cd2b64facaeb0d2f1b
Dranaen <sachama_@hotmail.com>
2021-08-26 12:16:42 +0200

    ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -35,6 +35,7 @@ import { ValidationAdminComponent } from './gestion-membre/validation-admin/vali
 import { RemerciementFormContactComponent } from './remerciement-form-contact/remerciement-form-contact.component';
 import { QuizzComponent } from './quizz/quizz.component';
 import { ExplorationComponent } from './exploration/exploration.component';
+import { SoleilComponent } from './soleil/soleil.component';
 
 @NgModule({
   declarations: [
@@ -60,7 +61,8 @@ import { ExplorationComponent } from './exploration/exploration.component';
     ValidationAdminComponent,
     RemerciementFormContactComponent,
     QuizzComponent,
-    ExplorationComponent
+    ExplorationComponent,
+    SoleilComponent
   ],
   imports: [
     BrowserModule,
