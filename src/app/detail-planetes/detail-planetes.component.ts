b511b1eb7a309598aff5b7efbdde2a344903af4d
jb <jbrx17@gmail.com>
2021-08-26 18:23:40 +0200

    article planete

--- src/app/detail-planetes/detail-planetes.component.ts
+++ src/app/detail-planetes/detail-planetes.component.ts
@@ -4,7 +4,7 @@ import { FormControl, FormGroup } from '@angular/forms';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { OtherService } from '../services/other.service';
 import { PlaneteService } from '../services/planete.service';
-
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-detail-planetes',
@@ -16,13 +16,13 @@ import { PlaneteService } from '../services/planete.service';
 export class DetailPlanetesComponent implements OnInit {
   satellites: any;
   planete: any;
-  taille:any;
-  filterName:any;
+  taille: any;
+  filterName: any;
   public buttonClicked: boolean = false;
-  constructor(private http: HttpClient, private otherService: OtherService) { }
+  constructor(private http: HttpClient, private otherService: OtherService, private route: Router,) { }
 
   ngOnInit(): void {
-   
+
     this.satelliteByPlanet();
     this.getPlanet();
   }
@@ -38,21 +38,24 @@ export class DetailPlanetesComponent implements OnInit {
       next: (data) => { this.planete = data },
       error: (err) => { console.log(err) }
     });
-    this.taille=this.planete.diamater/12753;
+    this.taille = this.planete.diamater / 12753;
   }
 
-  enregistrer(value:any): void{
-    this.planete.text=value
-    this.http.put(this.otherService.lienBack+'planet/article/'+PlaneteService.getPlanetes(),this.planete).subscribe({
+  enregistrer(value: any): void {
+    this.planete.text = value
+    this.http.put(this.otherService.lienBack + 'planet/article/' + PlaneteService.getPlanetes(), this.planete).subscribe({
       next: (data) => { this.planete = data },
       error: (err) => { console.log(err) }
     });
     this.buttonClicked = !this.buttonClicked;
   }
-  
+
   public onButtonClick() {
 
     this.buttonClicked = !this.buttonClicked;
-}
-  
+  }
+  gotosatellite(s: any){
+    PlaneteService.saveSatellite(s.id);
+    this.route.navigateByUrl("detail-satellites")
+  }
 }
