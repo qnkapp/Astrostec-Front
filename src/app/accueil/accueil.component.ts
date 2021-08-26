f95d5b46e9fb0f1a005a73fd4a3909ca33de786a
LaetitiaHinry <laetitia.hinry@gmail.com>
2021-08-26 07:09:08 +0200

    form adaptée et reliée à la méthode onSubmit

--- src/app/accueil/accueil.component.ts
+++ src/app/accueil/accueil.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -15,6 +16,10 @@ export class AccueilComponent implements OnInit {
 
   goToSource() : any{
     this.route.navigateByUrl('source');
+  }
+
+  onSubmit(contactForm : NgForm){
+
   }
 
 }
