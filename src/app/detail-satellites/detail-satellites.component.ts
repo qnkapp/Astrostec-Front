429d06043efcad527c4abb9798e303a42730a07f
jb <jbrx17@gmail.com>
2021-08-26 18:23:57 +0200

    article satellites

--- src/app/detail-satellites/detail-satellites.component.ts
+++ src/app/detail-satellites/detail-satellites.component.ts
@@ -8,10 +8,11 @@ import { PlaneteService } from '../services/planete.service';
   templateUrl: './detail-satellites.component.html',
   styleUrls: ['./detail-satellites.component.css']
 })
+
 export class DetailSatellitesComponent implements OnInit {
   satellites: any;
   planete: any;
- 
+  public buttonClicked: boolean = false;
   constructor(private http: HttpClient, private otherService: OtherService) { }
   
   ngOnInit(): void {
@@ -23,12 +24,24 @@ export class DetailSatellitesComponent implements OnInit {
       next: (data) => { this.satellites = data },
       error: (err) => { console.log(err) }
     });
-    
   }
   getPlanet(): void {
-    this.http.get(this.otherService.lienBack + 'planet/' + this.satellites.planetid).subscribe({
+    this.http.get(this.otherService.lienBack + 'planet/' + 3).subscribe({
       next: (data) => { this.planete = data },
       error: (err) => { console.log(err) }
     });
+  }
+  enregistrerSatellite(value: any): void {
+    this.satellites.text = value
+    this.http.put(this.otherService.lienBack + 'satellite/article/' + PlaneteService.getSatellite(), this.satellites).subscribe({
+      next: (data) => { this.satellites = data },
+      error: (err) => { console.log(err) }
+    });
+    this.buttonClicked = !this.buttonClicked;
+  }
+
+  public onButtonClick() {
+
+    this.buttonClicked = !this.buttonClicked;
   }
 }
