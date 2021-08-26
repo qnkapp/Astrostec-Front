081da96b606691cf430502661478590500032d8c
Quentin Lecroart <qnkapplication@gmail.com>
2021-08-26 16:44:10 +0200

    ajout produit ok

--- src/app/gestion-produits/gestion-produits.component.ts
+++ src/app/gestion-produits/gestion-produits.component.ts
@@ -11,30 +11,30 @@ import { OtherService } from '../services/other.service';
 })
 export class GestionProduitsComponent implements OnInit {
 
-  produits:any;
+  produits: any;
 
-  constructor(private http:HttpClient,private otherService:OtherService,private matDialog:MatDialog) { }
+  constructor(private http: HttpClient, private otherService: OtherService, private matDialog: MatDialog) { }
 
   ngOnInit(): void {
     this.getProducts();
   }
 
-  getProducts():void{
-    this.http.get(this.otherService.lienBack +'produit').subscribe({
-      next: (data) => {this.produits =data;console.log(data)},
-      error: (err) => { console.log(err)}
+  getProducts(): void {
+    this.http.get(this.otherService.lienBack + 'produit').subscribe({
+      next: (data) => { this.produits = data; console.log(data) },
+      error: (err) => { console.log(err) }
     });
   }
 
-  parseFloat(prix:any):number{
+  parseFloat(prix: any): number {
     return prix.toFixed(2);
   }
 
-  goToAdd(){
-    this.matDialog.open(AddProductComponent,{
-      height:'600px',
-      width:'500px',
-    })
+  goToAdd() {
+    var dialog = this.matDialog.open(AddProductComponent, {
+      height: '600px',
+      width: '500px',
+    }).afterClosed().subscribe(() => { this.getProducts();console.log(this.produits) });;
   }
 
 }
